b297bc4ce23e60ff4f3704b4ec075be4509b1631
ginolee <ginolee22@outlook.com>
2017-03-01 21:58:24 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,21 @@
 class Movie < ActiveRecord::Base
+    # define a field of all different ratings
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    def self.all_ratings 
+      @all_ratings
+    end
+    
+    # define the sorting method. sort is the sorting based on (eg. title, releasing date)
+    def self.sorting(sort)
+     if sort==nil
+        self
+     else
+        self.order(sort)
+     end
+    end
+    
+    def self.rating(rating)
+        self.where('rating in (?)', rating)
+    end
+    
 end
